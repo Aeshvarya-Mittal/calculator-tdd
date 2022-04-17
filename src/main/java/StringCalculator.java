47e941a4b497ec3343d66efc375f9d8ee06bd885
Aeshvarya Mittal <aeshvarya@hira.one>
2022-04-18 05:05:57 +0530

    Aeshvarya|extracting int from string and adding

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -4,6 +4,27 @@ public class StringCalculator {
         if (text.equals("")) {
             return 0;
         }
-        return 1;// returns value for all other cases
+        else{
+            String delimiter = ",";
+            String numList[] = getNumbers(text, delimiter);
+            return sum(numList);
+        }
+    }
+
+    private static String[] getNumbers(String numbers, String divider){
+        return numbers.split(divider);
+    }
+    private static int toInt(String number){
+        return Integer.parseInt(number);
+    }
+
+    private static int sum(String[] numbers){
+        int total = 0;
+
+        for(String number : numbers){
+            if(toInt(number)>0)
+                total += toInt(number);
+        }
+        return total;
     }
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -8,4 +8,19 @@ public class StringCalculatorTest {
     public void emptyStringTest() {
         assertEquals(0, StringCalculator.add(""));
     }
+
+    @Test
+    public void oneNumberTest() {
+        assertEquals(1, StringCalculator.add("1"));
+    }
+
+    @Test
+    public void twoNumbersTest(){
+        assertEquals(3, StringCalculator.add("1,2"));
+    }
+
+    @Test
+    public void threeNumbersTest(){
+        assertEquals(6, StringCalculator.add("1,2,3"));
+    }
 }
